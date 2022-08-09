13942fea56bbc9b3c6f2e8746f668f8b595f819d
MargaritaVin <ritasergienko@rambler.ru>
2022-08-09 22:09:08 +0300

    added function isWeekend in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+function isWeekend(date) {
+    const weekDay = new Date(date);
+    if (weekDay.getDay() === 6 || weekDay.getDay() === 0) {
+        return true ;
+    } else {
+        return false;
+    }
+};
+console.log(isWeekend('2022-02-12')); 
+console.log(isWeekend('2022-02-13')); 
+console.log(isWeekend('2022-02-09')); 
