349118b19ba046bc8448ee951dd518acc7cb4023
MargaritaVin <ritasergienko@rambler.ru>
2022-08-09 22:24:09 +0300

    added object person in script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,17 @@ function isWeekend(date) {
 console.log(isWeekend('2022-02-12')); 
 console.log(isWeekend('2022-02-13')); 
 console.log(isWeekend('2022-02-09')); 
+
+
+const person = {
+    fullName: 'Sherlock Holmes',
+    address: {
+        street: "Baker Street",
+        city: "London",
+        house: "221b"
+    }
+};
+
+let clonePerson = JSON.parse(JSON.stringify(person));
+
+const { fullName, address: {street, city, house} } = person;
