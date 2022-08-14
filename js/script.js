87cc5bd71e0baaac2d87baa7dc49240379217266
MargaritaVin <ritasergienko@rambler.ru>
2022-08-14 19:57:38 +0300

    added function getDiffDays in script.js

--- js/script.js
+++ js/script.js
@@ -29,3 +29,22 @@ const { fullName, address: {street, city, house} } = person;
 const dateBirthday = new Date(1994, 4, 28);
 let strDateBirthday = `${dateBirthday.getDate()}.${`0` + (dateBirthday.getMonth() + 1)}.${dateBirthday.getFullYear()}`;
 console.log("My birthday: " + strDateBirthday);
+
+function getDiffDays (start, end) {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    let daysBetweenStartAndEnd = (endDate - startDate) / 86400 / 1000;
+    
+    if (startDate.toString() === "Invalid Date" || endDate.toString() === "Invalid Date") {
+        return console.error("Error: invalid date");
+    } if (startDate > endDate) {
+        return console.error("Error: your start date is later than end");
+    } else {
+        return daysBetweenStartAndEnd;
+    }; 
+};
+
+console.log(getDiffDays('2020-01-01', '2020-01-17')); 
+console.log(getDiffDays('2020-01-01', '2020-03-15')); 
+console.log(getDiffDays('2222222222', '2020-01-17')); 
+console.log(getDiffDays('2021-01-02', '2020-03-15')); 
